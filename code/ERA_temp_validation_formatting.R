1755787000e2b31cca08f4f029c4a0622bfa8c63
ddlawton <ddlawton@asu.edu>
2022-02-23 12:18:40 -0500

    adding a urban site comparison for the JB modeling endeavor.

--- code/ERA_temp_validation_formatting.R
+++ code/ERA_temp_validation_formatting.R
@@ -48,24 +48,56 @@ dat <- rbind(dat_5cm,dat_10cm,dat_20cm,dat_50cm,dat_100cm) %>%
            depth == "10" ~ "soil_temperature_level_2",
            depth == "20" ~ "soil_temperature_level_2",
            depth == "50" ~ "soil_temperature_level_3",
-           depth == "100" ~ "soil_temperature_level_3"
-         ))
+           depth == "100" ~ "soil_temperature_level_3"),
+         location = "rural") %>%
+  rename(year = Year,temp = temps)
 
+
+Logger_UMB_dat <- read_excel("data/StPaulSOilTemp1996_2015_Clean-checked-Ebbenga-wdh.xlsx",sheet=1)[,1:8] %>%
+  pivot_longer(cols = starts_with("Average"),names_to = "depth", values_to = "temp") %>%
+  mutate(depth = factor(parse_number(depth))) %>%
+  group_by(year,month,depth) %>%
+  summarize(temp = mean(temp)) %>%
+  mutate(ERA_depth = case_when(
+    depth == "1" ~ "soil_temperature_level_1",
+    depth == "5" ~ "soil_temperature_level_2",
+    depth == "10" ~ "soil_temperature_level_2",
+    depth == "20" ~ "soil_temperature_level_3",
+    depth == "40" ~ "soil_temperature_level_3"
+  ),
+  location = "urban")
+
+combined_logger_dat <- rbind(Logger_UMB_dat,dat)
+  
+  
 # Relating logger data to remotely sensed data
 
-ERA_dat <- read.csv("data/MN_temps_all.csv") %>% #reading in the data and getting it into the format of the logger data
-  as_tibble() %>%
-  mutate(date = substr(system.index,1,6)) %>% #system.index is from google earth engine and it is YYYYMM.
-  select(!c(.geo,system.index)) %>%
+ERA_dat <- read_csv("data/MN_temps_all.csv") %>% #reading in the data and getting it into the format of the logger data
+  mutate(date = substr(`system:index`,1,6)) %>% #system.index is from google earth engine and it is YYYYMM.
+  select(!c(.geo,`system:index`)) %>%
   separate(date,into=c("Year","month"),sep=4) %>%
   mutate(month = as.integer(month), Year = as.integer(Year)) %>%
-  pivot_longer(cols=starts_with("soil"),names_to = "ERA_depth",values_to = "ERA_temps")
+  pivot_longer(cols=starts_with("soil"),names_to = "ERA_depth",values_to = "ERA_temps") %>%
+  mutate(location = "rural")
+
+RS_UMN_dat <- read_csv("data/UMN_temps.csv")  %>% #reading in the data and getting it into the format of the logger data
+  mutate(date = substr(`system:index`,1,6)) %>% #system.index is from google earth engine and it is YYYYMM.
+  select(!c(.geo,`system:index`)) %>%
+  separate(date,into=c("Year","month"),sep=4) %>%
+  mutate(month = as.integer(month), Year = as.integer(Year)) %>%
+  pivot_longer(cols=starts_with("soil"),names_to = "ERA_depth",values_to = "ERA_temps") %>%
+  mutate(location = "urban") %>%
+  select(!name)
+
+RS_dat <- rbind(ERA_dat,RS_UMN_dat) %>%
+  rename(year = Year)
+
 
 # now combining the two
 
-combined_dat <- dat %>% left_join(ERA_dat,by=c("Year","month","ERA_depth"))
+combined_dat <- combined_logger_dat %>% left_join(RS_dat,by=c("location","year","month","ERA_depth"))
 
-write.csv(combined_dat,file="data/processed/ERA_logger_data_validation.csv")
+write.csv(combined_dat,file="data/processed/ERA_logger_data_validation_site_comparison_Feb232022.csv")
 
 
 
--- code/modeling_ERA_and_logger_data.R
+++ code/modeling_ERA_and_logger_data.R
@@ -14,29 +14,56 @@ library(mgcv)
 library(gratia)
 library(DHARMa)
 
-dat <- read.csv("data/processed/ERA_logger_data_validation.csv") %>% as_tibble() %>%
+dat <- read.csv("data/processed/ERA_logger_data_validation_site_comparison_Feb232022.csv") %>% as_tibble() %>%
   drop_na(ERA_temps) %>% mutate(depth = factor(depth))
 
 
-ggplot(dat,aes(x=temps,y=ERA_temps,color=depth)) + geom_point(size=.75,alpha=.75,pch=21) + geom_smooth(se=FALSE,size=1.5) +
+ggplot(dat,aes(x=temp,y=ERA_temps,color=depth)) + geom_point(size=.75,alpha=.75,pch=21) + geom_smooth(se=FALSE,size=1.5) + #theres a weird 200+ point in the data. Lets remove it.
   facet_wrap(~depth)
 
-mod <- bam(temps ~ te(ERA_temps,depth,bs="fs",k=20),select=TRUE,data=dat)
+dat <- dat %>% filter(temp <100)
 
-mod_lm <- bam(temps ~ ERA_temps*depth,select=TRUE,data=dat)
+
+
+raw_data_graph <- ggplot(dat,aes(y=temp,x=ERA_temps,color=depth)) + geom_point(size=.75,alpha=.75,pch=21) + geom_smooth(method="gam",se=FALSE,size=1.5) + #much better!
+  facet_grid(depth~location) + 
+  ylab("Logger Temperature Data (C)") +
+  xlab("ERA Temperature Data (C)")
+
+
+ggsave(raw_data_graph,file="output/raw_data_graph.png",width=20,height=20,units="in",dpi=600)
+
+rural_dat <- dat %>% filter(location == "rural")
+urban_dat <- dat %>% filter(location == "urban")
+
+
+mod_rural <- bam(temp ~ te(ERA_temps,depth,bs="fs",k=20),select=TRUE,data=rural_dat,family=scat())
+mod_urban <- bam(temp ~ te(ERA_temps,depth,bs="fs",k=20),select=TRUE,data=urban_dat,family=scat())
+
+mod_rural_lm <- bam(temp ~ ERA_temps*depth,select=TRUE,data=rural_dat)
+mod_urban_lm <- bam(temp ~ ERA_temps*depth,select=TRUE,data=urabn_dat)
 
 # Both AIC and BIC selected for the non-linear model
-AIC(mod,mod_lm)
-BIC(mod,mod_lm)
+AIC(mod_rural,mod_rural_lm) # GAM
+AIC(mod_urban,mod_urban_lm) # GAM
+BIC(mod_rural,mod_rural_lm) # GAM
+BIC(mod_urban,mod_urban_lm) # GAM
 
 
-summary(mod)
-sim_resid <- simulateResiduals(mod)
+summary(mod_rural)
+sim_resid <- simulateResiduals(mod_rural)
 plot(sim_resid)
 
-b0 <- coef(mod)[1]
 
-test <- gratia::smooth_estimates(mod)
+summary(mod_urban)
+sim_resid <- simulateResiduals(mod_urban) #its not as pretty as the rural model, but will do.
+plot(sim_resid)
+
+# Rural model figures
+
+b0 <- coef(mod_rural)[1]
+
+test <- gratia::smooth_estimates(mod_rural)
 
 test$adj_est <- test$est + b0
 
@@ -51,7 +78,7 @@ plotted_together <- test %>% filter(smooth == "te(ERA_temps,depth)") %>%
   xlim(0,30)
 
 grid <- test %>% filter(smooth == "te(ERA_temps,depth)") %>%
-  ggplot(aes(x=ERA_temps,group=depth)) + #geom_ribbon(aes(ymin=adj_est-se, ymax=adj_est+se,fill=depth),alpha=.1) +
+  ggplot(aes(x=ERA_temps,group=depth)) + geom_ribbon(aes(ymin=adj_est-se, ymax=adj_est+se,fill=depth),alpha=.1) +
   geom_line(aes(y = adj_est, color=depth),size=1.5) + theme_pubr() +
   scale_color_manual(values = c('#9e9ac8','#807dba','#6a51a3','#54278f','#3f007d')) +
   scale_fill_manual(values = c('#9e9ac8','#807dba','#6a51a3','#54278f','#3f007d')) + 
@@ -61,5 +88,38 @@ grid <- test %>% filter(smooth == "te(ERA_temps,depth)") %>%
   xlim(0,30) +
   facet_wrap(~depth)
 
-ggsave(plotted_together,file="output/lines_together.png",width=10,height=10,units="in",dpi=600)
-ggsave(grid,file="output/gridded.png",width=10,height=10,units="in",dpi=600)
+ggsave(plotted_together,file="output/lines_together_rural.png",width=10,height=10,units="in",dpi=600)
+ggsave(grid,file="output/gridded_rural.png",width=10,height=10,units="in",dpi=600)
+
+# Urban model figures
+
+b0 <- coef(mod_urban)[1]
+
+test <- gratia::smooth_estimates(mod_urban)
+
+test$adj_est <- test$est + b0
+
+
+plotted_together <- test %>% filter(smooth == "te(ERA_temps,depth)") %>%
+  ggplot(aes(x=ERA_temps,group=depth)) + #geom_ribbon(aes(ymin=adj_est-se, ymax=adj_est+se,fill=depth),alpha=.1) +
+  geom_line(aes(y = adj_est, color=depth),size=1.5) + theme_pubr() +
+  scale_color_manual(values = c('#fdae6b','#fd8d3c','#f16913','#d94801','#8c2d04')) +
+  scale_fill_manual(values = c('#fdae6b','#fd8d3c','#f16913','#d94801','#8c2d04')) + 
+  xlab("ERA climatic reanalysis temperature (C)") +
+  ylab("Modeled soil logger temperature (C)") +
+  ylim(0,30) +
+  xlim(0,30)
+
+grid <- test %>% filter(smooth == "te(ERA_temps,depth)") %>%
+  ggplot(aes(x=ERA_temps,group=depth)) + geom_ribbon(aes(ymin=adj_est-se, ymax=adj_est+se,fill=depth),alpha=.1) +
+  geom_line(aes(y = adj_est, color=depth),size=1.5) + theme_pubr() +
+  scale_color_manual(values = c('#fdae6b','#fd8d3c','#f16913','#d94801','#8c2d04')) +
+  scale_fill_manual(values = c('#fdae6b','#fd8d3c','#f16913','#d94801','#8c2d04')) + 
+  xlab("ERA climatic reanalysis temperature (C)") +
+  ylab("Modeled soil logger temperature (C)") +
+  ylim(0,30) +
+  xlim(0,30) +
+  facet_wrap(~depth)
+
+ggsave(plotted_together,file="output/lines_together_urban.png",width=10,height=10,units="in",dpi=600)
+ggsave(grid,file="output/gridded_urban.png",width=10,height=10,units="in",dpi=600)
